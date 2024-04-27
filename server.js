8a19280b0d699c4bffa0bf9523d2f3e191a9a68d
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-04-27 18:24:50 +0200

    Add components to server
    - Add routers
    - Add errorHandler middleware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import { sendMailRouter } from "./router/sendMailRouter.js";
+import { receiveMailRouter } from "./router/receiveMailRouter.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 const PORT = 8080;
@@ -8,7 +11,11 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/mail", sendMailRouter, receiveMailRouter);
+
 app.use("*", (req, res) => res.sendStatus(404));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
