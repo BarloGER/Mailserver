160d5e4cda3ef28815dace12310c125c416bc3d1
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-04-29 15:20:12 +0200

    Delete unnecessary files
    - Delete schemas
    - Delete routers

--- router/receiveMailRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { validateJoi } from "../middlewares/validateJoi.js";
-import { contactSchema } from "../joi/contactSchema.js";
-
-import { contact } from "../controllers/mailController.js";
-
-export const receiveMailRouter = Router();
-
-receiveMailRouter.post("/receive/contact", validateJoi(contactSchema), contact);
